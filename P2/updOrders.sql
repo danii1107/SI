2b6065156606c2b3c3fda5966cc0fa5db0d48f24
danii1107 <danielbarsan72003@gmail.com>
2023-11-15 13:27:49 +0100

    f done

--- P2/updOrders.sql
+++ P2/updOrders.sql
@@ -1,20 +1,34 @@
-CREATE OR REPLACE FUNCTION updOrders() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION updOrders() 
+RETURNS TRIGGER as $$
+DECLARE
+    extra int4;
 BEGIN
-    PERFORM setOrderAmount();
-    RETURN NEW;
+    IF (TG_OP = 'INSERT') THEN
+        extra := (select o.netamount from orders o where o.orderid = new.orderid);
+        UPDATE orders set netamount = extra + (new.price*new.quantity) where orders.orderid = new.orderid;
+        UPDATE orders set totalamount = (netamount + (netamount*tax/100)) where orders.orderid = new.orderid;
+    ELSEIF (TG_OP = 'DELETE') THEN
+        extra := (select o.netamount from orders o where o.orderid = old.orderid);
+        UPDATE orders set netamount = extra - (old.price*old.quantity) where orders.orderid = old.orderid;
+        UPDATE orders set totalamount = (netamount + (netamount*tax/100)) where orders.orderid = old.orderid;
+    ELSEIF (TG_OP = 'UPDATE') THEN
+        extra := (select o.netamount from orders o where o.orderid = old.orderid);
+        extra := extra - (old.price*old.quantity);
+        UPDATE orders set netamount = extra + (new.price*new.quantity) where orders.orderid = new.orderid;
+        UPDATE orders set totalamount = (netamount + (netamount*tax/100)) where orders.orderid = new.orderid;
+    END IF;
+    RETURN NULL;
 END;
-$$ LANGUAGE plpgsql;
+    $$ 
+LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE TRIGGER updOrders
-AFTER INSERT OR DELETE OR UPDATE ON orderdetail
+AFTER DELETE OR INSERT OR UPDATE ON orderdetail
 FOR EACH ROW
-    EXECUTE PROCEDURE updOrders();
+EXECUTE PROCEDURE updOrders();
 
 
-
-	
-
-UPDATE orderdetail SET price = 200 WHERE orderid = 99997;
+UPDATE orderdetail SET price = 33 WHERE orderid = 99997;
 
 SELECT * FROM orders o WHERE o.orderid = 99997;
 
